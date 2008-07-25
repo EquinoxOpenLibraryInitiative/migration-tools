896725a108864a4de6e6ea72b91ada9e1e7a933d
Jason Etheridge <jason@esilibrary.com>
2008-07-25 19:20:05 +0000

    more unicorn conversion tools

---
+++ transform_unicorn_flat_charges.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl -w
+
+my $count = 0;
+my %records = ();
+
+sub print_line {
+    print join("\t",
+        $records{ $count }{'FORM'} || '',
+        $records{ $count }{'USER_ID'} || '',
+        $records{ $count }{'ITEM_ID'} || '',
+        $records{ $count }{'CHRG_LIBRARY'} || '',
+        $records{ $count }{'CHRG_DC'} || '',
+        $records{ $count }{'CHRG_DATEDUE'} || '',
+    ) . "\n"; 
+}
+
+print "FORM\tUSER_ID\tITEM_ID\tCHRG_LIBRARY\tCHRG_DC\tCHRG_DATEDUE\n";
+
+while (my $line = <>) {
+    chomp $line; $line =~ s/[\r\n]//g;
+    if ($line =~ /DOCUMENT BOUNDARY/) {
+        if (defined $records{ $count }) {
+            print_line();
+        }
+        $count++; $records{ $count } = {};
+    }
+    if ($line =~ /FORM=(.+)/) {
+        $records{ $count }{'FORM'} = $1;
+    }
+    if ($line =~ /\.USER_ID\..+\|a(.+)/) {
+        $records{ $count }{'USER_ID'} = $1;
+    }
+    if ($line =~ /\.ITEM_ID\..+\|a(.+)/) {
+        $records{ $count }{'ITEM_ID'} = $1;
+    }
+    if ($line =~ /\.CHRG_LIBRARY\..+\|a(.+)/) {
+        $records{ $count }{'CHRG_LIBRARY'} = $1;
+    }
+    if ($line =~ /\.CHRG_DC\..+\|a(.+)/) {
+        $records{ $count }{'CHRG_DC'} = $1;
+    }
+    if ($line =~ /\.CHRG_DATEDUE\..+\|a(.+)/) {
+        $records{ $count }{'CHRG_DATEDUE'} = $1;
+    }
+}
+print_line();
+
