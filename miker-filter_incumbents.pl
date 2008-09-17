81946b59bfb937b324221c5910be127a762e1941
Shawn Boyette <sboyette@esilibrary.com>
2008-09-17 21:51:24 +0000

    adding yaz-cleanup, a small tool to scrub data from yaz-marcdump

--- miker-filter_incumbents.pl
+++ miker-filter_incumbents.pl
@@ -1,8 +1,15 @@
 #!/usr/bin/perl
+use warnings;
+use strict;
 
-use Time::HiRes qw/time/;
-use MARC::Record;
-use MARC::File::XML ( BinaryEncoding => 'utf-8' );
+use Getopt::Long;
+#use Time::HiRes qw/time/;
+#use MARC::Record;
+#use MARC::File::XML ( BinaryEncoding => 'utf-8' );
+
+# configuration hashref
+my $conf  = ();
+initialize($conf);
 
 my $idfile = shift;
 my $marcfile = shift;
@@ -29,20 +36,84 @@ my $count = 0;
 my $icount = 0;
 my $scount = 0;
 while (<$M>) {
+    /tag="901" ind1=" " ind2=" "><subfield code="a">(\d+)</;
+    if ( $id{$1} ) {
+        print $I $_;
+        $icount++;
+    } else {
+        print $S $_;
+        $scount++;
+    }
+    $count++;
 
-	/tag="901" ind1=" " ind2=" "><subfield code="a">(\d+)</;
-	if ( $id{$1} ) {
-		print $I $_;
-		$icount++;
-	} else {
-		print $S $_;
-		$scount++;
-	}
-	$count++;
+    unless ($count && $count % 100) {
+        print STDERR "\r$count\t(shelved: $scount, import: $icount)\t". $count / (time - $starttime);
+    }
+}
 
-	unless ($count && $count % 100) {
-		print STDERR "\r$count\t(shelved: $scount, import: $icount)\t". $count / (time - $starttime);
-	}
+=head2 initialize
+
+Performs boring script initialization. Handles argument parsing,
+mostly.
+
+=cut
+
+sub initialize {
+    my ($c) = @_;
+    my @missing = ();
+
+    # set mode on existing filehandles
+    binmode(STDIN, ':utf8');
+
+    my $rc = GetOptions( $c,
+                         'incoming',
+                         'incumbent',
+                         'incoming-tag|incot=i',
+                         'incoming-subfield|incos=s',
+                         'incumbent-tag|incut=i',
+                         'incumbent-subfield|incus=s',
+                         'output|o=s',
+                         'help|h',
+                       );
+    show_help() unless $rc;
+    show_help() if ($c->{help});
+
+    $c->{'incoming-tag'}         = 903;
+    $c->{'incoming-subfield'}    = 'a';
+    $c->{'incoming-matchfile'}   = '';
+    $c->{'incoming-nomatchfile'} = '';
+    $c->{'incumbent-tag'}         = 901;
+    $c->{'incumbent-subfield'}    = 'a';
+    $c->{'incumbent-matchfile'}   = '';
+    $c->{'incumbent-nomatchfile'} = '';
+    my @keys = keys %{$c};
+    show_help() unless (@ARGV and @keys);
+    for my $key ('renumber-from', 'tag', 'subfield', 'output')
+      { push @missing, $key unless $c->{$key} }
+    if (@missing) {
+        print "Required option: ", join(', ', @missing), " missing!\n";
+        show_help();
+    }
 
 }
-		
+
+
+=head2 show_help
+
+Display usage message when things go wrong
+
+=cut
+
+sub show_help {
+print <<HELP;
+Usage is: $0 [REQUIRED ARGS]
+Req'd Arguments
+  --renumber-from=N        -rf First id# of new sequence
+  --tag=N                  -t  Which tag to use
+  --subfield=X             -s  Which subfield to use
+  --output=<file>          -o  Output filename
+
+Any number of input files may be specified; one output file will result.
+HELP
+exit 1;
+}
