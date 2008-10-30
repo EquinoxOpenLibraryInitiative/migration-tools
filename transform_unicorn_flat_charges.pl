68d8436f22b4ff2c063e2ff61e2f0ff64906424e
Jason Etheridge <jason@esilibrary.com>
2008-10-30 01:22:38 +0000

    support CHRG_DATE_CLMRET

--- transform_unicorn_flat_charges.pl
+++ transform_unicorn_flat_charges.pl
@@ -11,10 +11,11 @@ sub print_line {
         $records{ $count }{'CHRG_LIBRARY'} || '',
         $records{ $count }{'CHRG_DC'} || '',
         $records{ $count }{'CHRG_DATEDUE'} || '',
+        $records{ $count }{'CHRG_DATE_CLMRET'} || '',
     ) . "\n"; 
 }
 
-print "FORM\tUSER_ID\tITEM_ID\tCHRG_LIBRARY\tCHRG_DC\tCHRG_DATEDUE\n";
+print "FORM\tUSER_ID\tITEM_ID\tCHRG_LIBRARY\tCHRG_DC\tCHRG_DATEDUE\nCHRG_DATE_CLMRET\n";
 
 while (my $line = <>) {
     chomp $line; $line =~ s/[\r\n]//g;
@@ -41,6 +42,9 @@ while (my $line = <>) {
     }
     if ($line =~ /\.CHRG_DATEDUE\..+\|a(.+)/) {
         $records{ $count }{'CHRG_DATEDUE'} = $1;
+    }
+    if ($line =~ /\.CHRG_DATEDUE\..+\|a(.+)/) {
+        $records{ $count }{'CHRG_DATE_CLMRET'} = $1;
     }
 }
 print_line();
