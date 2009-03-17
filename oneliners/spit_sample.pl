c3c9f9574214df27a14aa911adf1349e47feee20
Shawn Boyette <sboyette@esilibrary.com>
2009-03-17 16:57:11 +0000

    moving stuff, again, finishing

--- oneliners/spit_sample.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-my @lines = <>;
-
-foreach my $i ( 1..20 ) {
-    $length = scalar( @lines );
-    $idx = int rand ($length);
-    print $lines[$idx];
-    splice(@lines,$idx,1);
-}
