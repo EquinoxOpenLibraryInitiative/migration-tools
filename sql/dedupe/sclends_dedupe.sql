ed40519fc98ff064e4754df4cbd2f766bc24e9cf
Galen Charlton <gmc@esilibrary.com>
2011-06-16 23:10:16 -0400

    add license statement
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- sql/dedupe/sclends_dedupe.sql
+++ sql/dedupe/sclends_dedupe.sql
@@ -1,7 +1,21 @@
 -- SCLENDS bibliographic dedupe routine
 --
 -- Copyright 2010-2011 Equinox Software, Inc.
--- Author: Galen Charlton
+-- Author: Galen Charlton <gmc@esilibrary.com>
+-- 
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2, or (at your option)
+-- any later version.
+-- 
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+-- 
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
 --
 -- This implements a bibliographic deduplication routine based
 -- on criteria and an algorithm specified by the South Carolina
