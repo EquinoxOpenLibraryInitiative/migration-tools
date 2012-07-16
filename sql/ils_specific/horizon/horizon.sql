7af21df77fb31c255c21c4a37addf7c645d42731
Galen Charlton <gmc@esilibrary.com>
2012-07-16 11:53:35 -0400

    add copyright statement and GPL2+ license statement
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- sql/ils_specific/horizon/horizon.sql
+++ sql/ils_specific/horizon/horizon.sql
@@ -1,3 +1,19 @@
+-- Copyright 2009-2012, Equinox Software, Inc.
+--
+-- This program is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License
+-- as published by the Free Software Foundation; either version 2
+-- of the License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 CREATE OR REPLACE FUNCTION migration_tools.attempt_hz_date (TEXT,TEXT) RETURNS DATE AS $$
     DECLARE
         attempt_value ALIAS FOR $1;
