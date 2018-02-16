6a30f309b7e68358c4c8b8c6dc8d18c6331c3a81
Rogan Hamby <rhamby@esilibrary.com>
2018-02-16 14:15:51 -0500

    adjusted universal files to no longer need fields from tracked tables that were dropped

--- mig-sql/init/010-gsheet_tracked_table.sql
+++ mig-sql/init/010-gsheet_tracked_table.sql
@@ -1,12 +1,10 @@
 CREATE TABLE gsheet_tracked_table (
     id                          SERIAL PRIMARY KEY
-    ,worksheet_name             TEXT NOT NULL
-    ,worksheet_key              TEXT
     ,table_name                 TEXT NOT NULL
     ,tab_name                   TEXT
     ,created                    TIMESTAMP
     ,last_pulled                TIMESTAMP
     ,last_pushed                TIMESTAMP
-    ,UNIQUE(worksheet_name,tab_name)
+    ,UNIQUE(table_name,tab_name)
 );
 
