8f3c70aa5aaeb1c4742c4d15da7fc535035b3a2e
Jason Etheridge <jason@esilibrary.com>
2018-10-02 13:49:45 -0400

    moar columns for bre
    Signed-off-by: Jason Etheridge <jason@esilibrary.com>

--- sql/templates/bib_fast_template.sql
+++ sql/templates/bib_fast_template.sql
@@ -1,3 +1,3 @@
 SET CLIENT_ENCODING TO 'UNICODE';
 
-\COPY biblio.record_entry (active,create_date,creator,deleted,edit_date,editor,fingerprint,id,last_xact_id,marc,quality,source,tcn_source,tcn_value,owner,share_depth) FROM 'XXX'
+\COPY biblio.record_entry (active,create_date,creator,deleted,edit_date,editor,fingerprint,id,last_xact_id,marc,quality,source,tcn_source,tcn_value,owner,share_depth,merge_date,merged_to) FROM 'XXX'
