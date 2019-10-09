ef6bbe8ca6c8324d2c393591b644f2a1cd26845d
Jason Etheridge <jason@equinoxinitiative.org>
2019-10-09 12:26:50 -0400

    the new base.sql

--- sql/base/base.sql
+++ sql/base/base.sql
@@ -1,31 +1,14 @@
--- Copyright 2009-2012, Equinox Software, Inc.
---
--- This program is free software; you can redistribute it and/or
--- modify it under the terms of the GNU General Public License
--- as published by the Free Software Foundation; either version 2
--- of the License, or (at your option) any later version.
---
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU General Public License for more details.
---
--- You should have received a copy of the GNU General Public License
--- along with this program; if not, write to the Free Software
--- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
---------------------------------------------------------------------------
--- An example of how to use:
--- 
--- DROP SCHEMA foo CASCADE; CREATE SCHEMA foo; 
--- \i base.sql
--- SELECT migration_tools.init('foo');
--- SELECT migration_tools.build('foo');
--- SELECT * FROM foo.fields_requiring_mapping;
--- \d foo.actor_usr
--- create some incoming ILS specific staging tables, like CREATE foo.legacy_items ( l_barcode TEXT, .. ) INHERITS (foo.asset_copy);
--- Do some mapping, like UPDATE foo.legacy_items SET barcode = TRIM(BOTH ' ' FROM l_barcode);
--- Then, to move into production, do: select migration_tools.insert_base_into_production('foo')
-
-CREATE SCHEMA migration_tools;
-
+\i 00-infrastructure.sql
+\i 01-marc.sql
+\i 02-barcodes.sql
+\i 03-items.sql
+\i 04-names.sql
+\i 05-addresses.sql
+\i 06-ils-specific.sql
+\i 07-eg-specific.sql
+\i 08-casting-and-validation.sql
+\i 09-misc.sql
+\i 10-staging.sql
+\i 21-fixed-fields.sql
+\i 22-anyarray.sql
+--\i 99-deprecated.sql
