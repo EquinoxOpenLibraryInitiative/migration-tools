9e78fa1a0e3d6c704c2a60deb0e29f4643f67d79
Rogan Hamby <rhamby@equinoxinitiative.org>
2020-09-24 13:35:36 -0400

    improvements and marking scripts as specific to m3 though they will probably work with other versions as well

--- mandarin/clean_mandarin_m3_hold_list_by_patron.pl
+++ mandarin/clean_mandarin_m3_hold_list_by_patron.pl
@@ -10,31 +10,32 @@ open(my $fh, '<:encoding(UTF-8)', $filename)
   or die "Could not open file '$filename' $!";
 
 my $user;
+
+print "user\titem\trequested\texpire\n";
  
 while (my $row = <$fh>) {
   chomp $row;
-  if ($row =~ m/Items On Loan by Patron/) { next; }
+  $row =~ s/ +/ /g;
+  if ($row =~ m/Items On Hold by Patron/) { next; }
   if ($row =~ m/Patron Title Barcode /) { next; }
   if ($row =~ m/Page: /) { next; }
   if ($row =~ m/Program Files \(x86\)/) { next; }
-  if ($row =~ m/End Of Report/) { next; }
-
+  if ($row =~ m/End Of Report/) { last; }
+  if ($row =~ m/^$/) { next; }
   my @str = split / /, $row;
   my $str_length = scalar(@str);
-  if ($str[1] eq 'AM' or $str[1] eq 'PM') {
-    if ($str[2] =~ m/2019/) { next; }	
-  }
+  if ($str[1] eq 'AM' or $str[1] eq 'PM') { next; }
+
 
   if ($str[$str_length -1] !~ m/\d\d\/\d\d\/\d\d\d\d/) {
     $user = $str[$str_length -1];
     next;
   }
 
-  #print "$row\n";
   if ($str[$str_length -1] =~  m/\d\d\/\d\d\/\d\d\d\d/) {
-    my $due       = $str[$str_length -1];  
-    my $borrowed  = $str[$str_length -2];  
+    my $posted    = $str[$str_length -1];  
+    my $expire    = $str[$str_length -2];  
     my $item      = $str[$str_length -3];  #item
-    print "$user\t$item\t$due\t$borrowed\n";
+    print "$user\t$item\t$posted\t$expire\n";
   } 
 }
--- mandarin/clean_mandarin_m3_loan_list_report.pl
+++ mandarin/clean_mandarin_m3_loan_list_report.pl
@@ -10,15 +10,19 @@ open(my $fh, '<:encoding(UTF-8)', $filename)
   or die "Could not open file '$filename' $!";
 
 my $user;
+
+print "user\titem\tduedate\tborrowed\n";
  
 while (my $row = <$fh>) {
   chomp $row;
-  if ($row =~ m/Fines Due by patron/) { next; }
-  if ($row =~ m/Patron Title Barcode Date Due Fine/) { next; }
+  $row =~ s/ +/ /g;
+  if ($row =~ m/^$/) { next; }
+  if ($row =~ m/^ $/) { next; }
+  if ($row =~ m/Items On Loan by Patron/) { next; }
+  if ($row =~ m/Patron Title Barcode /) { next; }
   if ($row =~ m/Page: /) { next; }
   if ($row =~ m/Program Files \(x86\)/) { next; }
   if ($row =~ m/End Of Report/) { next; }
-  if ($row =~ m/Fines Due by Patron/) { next; }
 
   my @str = split / /, $row;
   my $str_length = scalar(@str);
@@ -26,15 +30,16 @@ while (my $row = <$fh>) {
     if ($str[2] =~ m/2019/) { next; }	
   }
 
-  if ($str[$str_length -1] !~ m/^\d*\.?\d*$/) {
+  if ($str[$str_length -1] !~ m/\d\d\/\d\d\/\d\d\d\d/) {
     $user = $str[$str_length -1];
     next;
   }
 
   #print "$row\n";
-  if ($str[$str_length -1] =~  m/^\d*\.?\d*$/) {
-    my $f = $str[$str_length -1];  #fine
-    my $i = $str[$str_length -3];  #item barcode 
-    print "$user\t$i\t$f\n";
+  if ($str[$str_length -1] =~  m/\d\d\/\d\d\/\d\d\d\d/) {
+    my $due       = $str[$str_length -1];  
+    my $borrowed  = $str[$str_length -2];  
+    my $item      = $str[$str_length -3];  #item
+    print "$user\t$item\t$due\t$borrowed\n";
   } 
 }
