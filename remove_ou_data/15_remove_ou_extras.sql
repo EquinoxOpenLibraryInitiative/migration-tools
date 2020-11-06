cfbafe8fd0b0299f4dab7fd06638dc16a17f5b17
Rogan Hamby <rhamby@equinoxinitiative.org>
2020-11-06 11:15:46 -0500

    improving removing action trigger stuff

--- remove_ou_data/15_remove_ou_extras.sql
+++ remove_ou_data/15_remove_ou_extras.sql
@@ -52,30 +52,40 @@ WHERE btype IN (
     (SELECT (actor.org_unit_descendants(id)).id from actor.org_unit where shortname = :ou_to_del)
 );
 
-DELETE FROM action_trigger.event_output WHERE id IN (
-SELECT template_output FROM action_trigger.event 
+DROP TABLE IF EXISTS esi.temp_action_trigger_output_list;
+CREATE UNLOGGED TABLE esi.temp_action_trigger_output_list AS
+SELECT template_output AS output_id FROM action_trigger.event
 WHERE  event_def IN (
     SELECT id FROM action_trigger.event_definition
     WHERE owner IN
     (SELECT (actor.org_unit_descendants(id)).id from actor.org_unit where shortname = :ou_to_del)
-));
-
-DELETE FROM action_trigger.event_output WHERE id IN (
-SELECT async_output FROM action_trigger.event 
+)
+UNION ALL
+SELECT error_output AS output_id FROM action_trigger.event
 WHERE  event_def IN (
     SELECT id FROM action_trigger.event_definition
     WHERE owner IN
     (SELECT (actor.org_unit_descendants(id)).id from actor.org_unit where shortname = :ou_to_del)
-));
-
-DELETE FROM action_trigger.event_output WHERE id IN (
-SELECT error_output FROM action_trigger.event 
+)
+UNION ALL
+SELECT async_output AS output_id FROM action_trigger.event
 WHERE  event_def IN (
     SELECT id FROM action_trigger.event_definition
     WHERE owner IN
     (SELECT (actor.org_unit_descendants(id)).id from actor.org_unit where shortname = :ou_to_del)
-));
-
+);
+  
+UPDATE action_trigger.event SET template_output = NULL, error_output = NULL, async_output = NULL
+WHERE  event_def IN (
+    SELECT id FROM action_trigger.event_definition
+    WHERE owner IN
+    (SELECT (actor.org_unit_descendants(id)).id from actor.org_unit where shortname = :ou_to_del)
+);
+  
+DELETE FROM action_trigger.event_output WHERE id IN (
+    SELECT output_id FROM esi.temp_action_trigger_output_list
+);
+ 
 DELETE FROM action_trigger.event
 WHERE event_def IN (
     SELECT id FROM action_trigger.event_definition
@@ -94,10 +104,10 @@ WHERE event_def IN (
     WHERE owner IN
     (SELECT (actor.org_unit_descendants(id)).id from actor.org_unit where shortname = :ou_to_del)
 );
-
 DELETE FROM action_trigger.event_definition
     WHERE owner IN
     (SELECT (actor.org_unit_descendants(id)).id from actor.org_unit where shortname = :ou_to_del);
+
 
 COMMIT;
 DROP INDEX money.tmp_idx;
