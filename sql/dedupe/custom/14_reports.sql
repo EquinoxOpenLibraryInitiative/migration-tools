4209758d96166e066dec61dfa5b644a56a723918
Rogan Hamby <rhamby@equinoxinitiative.org>
2022-09-29 15:31:28 -0400

    adding copy count and circ mod summary to dedupe report

--- sql/dedupe/custom/14_reports.sql
+++ sql/dedupe/custom/14_reports.sql
@@ -1,38 +1,73 @@
 \timing on
 
+DROP TABLE IF EXISTS groups_copies;
+CREATE TABLE groups_copies AS 
+WITH bib_list AS (
+  SELECT DISTINCT id, bre_id FROM 
+  ( SELECT id, lead_record AS bre_id FROM groups
+    UNION ALL 
+    SELECT id, UNNEST(records) AS bre_id FROM groups
+  ) recs
+)
+SELECT 
+   acn.record
+  ,bl.id AS group_id 
+  ,acp.id AS acp_id 
+  ,COALESCE(acp.circ_modifier,'NONE') AS circ_mod
+FROM asset.call_number acn 
+JOIN bib_list bl ON bl.bre_id = acn.record 
+JOIN asset.copy acp ON acp.call_number = acn.id 
+WHERE NOT acp.deleted 
+;
+
+DROP TABLE IF EXISTS groups_copies_summary;
+CREATE TABLE groups_copies_summary AS 
+SELECT group_id, COUNT(acp_id) AS acp_count
+  ,ARRAY_AGG(DISTINCT circ_mod) AS circ_mods
+FROM groups_copies
+GROUP BY 1;
+
 \x off
 \pset format unaligned
 \f ','
 
 \o ~/dedupe_groups_all_report.csv
 SELECT 
-	'"' || g.lead_record::TEXT || '"' AS "Lead Record"
-	,'"' || b.o_title || '"' AS "Title"
-	,'"' || b.o_author || '"' AS "Author"
-	,'"' || ARRAY_TO_STRING(b.all_pubdates,'/') || '"' AS "Pubdates"
-	,'"' || ARRAY_TO_STRING(b.isbn_values,'/') || '"' AS "ISBNs"
-	,'"' || ARRAY_TO_STRING(b.upc_values,'/') || '"' AS "UPCs"
-    ,'"' || ARRAY_TO_STRING(g.merge_sets,'/') || '"' AS "Merge Sets"
-	,'"' || ARRAY_TO_STRING(g.records,' | ') || '"' AS "Subordinate Records"
-FROM 
-	groups g
-JOIN
-	dedupe_batch b ON b.record = g.lead_record
-;
-\o
-
-\o ~/dedupe_groups_morethan4_subordinates.csv
-SELECT
     '"' || g.lead_record::TEXT || '"' AS "Lead Record"
     ,'"' || b.o_title || '"' AS "Title"
     ,'"' || b.o_author || '"' AS "Author"
     ,'"' || ARRAY_TO_STRING(b.all_pubdates,'/') || '"' AS "Pubdates"
     ,'"' || ARRAY_TO_STRING(b.isbn_values,'/') || '"' AS "ISBNs"
     ,'"' || ARRAY_TO_STRING(b.upc_values,'/') || '"' AS "UPCs"
-    ,'"' || ARRAY_TO_STRING(g.merge_sets,'/') || '"' AS "Merge Sets"
+  ,'"' || ARRAY_TO_STRING(g.merge_sets,'/') || '"' AS "Merge Sets"
+  ,'"' || COALESCE(gcs.acp_count::TEXT,'No Copies') || '"' AS "Copy Counts"
+  ,'"' || COALESCE(ARRAY_TO_STRING(gcs.circ_mods,' | '),'No Copies') || '"' AS "Circ Mods"
     ,'"' || ARRAY_TO_STRING(g.records,' | ') || '"' AS "Subordinate Records"
-FROM
+FROM 
     groups g
+LEFT JOIN 
+  groups_copies_summary gcs ON gcs.group_id = g.id 
+JOIN
+    dedupe_batch b ON b.record = g.lead_record
+;
+\o
+
+\o ~/dedupe_groups_morethan4_subordinates.csv
+SELECT 
+    '"' || g.lead_record::TEXT || '"' AS "Lead Record"
+    ,'"' || b.o_title || '"' AS "Title"
+    ,'"' || b.o_author || '"' AS "Author"
+    ,'"' || ARRAY_TO_STRING(b.all_pubdates,'/') || '"' AS "Pubdates"
+    ,'"' || ARRAY_TO_STRING(b.isbn_values,'/') || '"' AS "ISBNs"
+    ,'"' || ARRAY_TO_STRING(b.upc_values,'/') || '"' AS "UPCs"
+  ,'"' || ARRAY_TO_STRING(g.merge_sets,'/') || '"' AS "Merge Sets"
+  ,'"' || COALESCE(gcs.acp_count::TEXT,'No Copies') || '"' AS "Copy Counts"
+  ,'"' || COALESCE(ARRAY_TO_STRING(gcs.circ_mods,' | '),'No Copies') || '"' AS "Circ Mods"
+    ,'"' || ARRAY_TO_STRING(g.records,' | ') || '"' AS "Subordinate Records"
+FROM 
+    groups g
+LEFT JOIN 
+  groups_copies_summary gcs ON gcs.group_id = g.id 
 JOIN
     dedupe_batch b ON b.record = g.lead_record
 WHERE 
