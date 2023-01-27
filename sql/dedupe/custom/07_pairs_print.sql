1b70babc53a48de55d0f12e7dca92ed6e016b9e4
Rogan Hamby <rogan.hamby@equinoxinitiative.org>
2023-01-27 11:45:30 -0500

    adding a more restrictive version of the isbn match set

--- sql/dedupe/custom/07_pairs_print.sql
+++ sql/dedupe/custom/07_pairs_print.sql
@@ -48,6 +48,41 @@ WHERE
     AND dedupe_setting('merge tacs isbn') = 'TRUE'
 ;
 
+-- stock isbn and publisher match set, more conservative than just isbn
+INSERT INTO pairs (merge_set,records,match_set)
+SELECT
+    'tacs isbn and pub info'
+    ,ARRAY[a.record,b.record]
+    ,a.title
+FROM
+    dedupe_batch a
+JOIN
+    dedupe_batch b ON b.title = a.title AND a.search_format_str = b.search_format_str
+WHERE
+    a.record != b.record
+    AND a.author = b.author
+    AND a.can_have_copies
+    AND b.can_have_copies
+    AND (
+          (a.avdisc_flag = FALSE AND b.avdisc_flag = FALSE)
+            OR
+          (a.avdisc_flag AND b.avdisc_flag AND (a.description = b.description OR (a.description IS NULL AND b.description IS
+NULL)))
+        )
+    AND a.isbn_values && b.isbn_values
+    AND ((a.all_pubdates && b.all_pubdates) OR (a.all_pubdates IS NULL AND b.all_pubdates IS NULL))
+    AND ((a.all_publishers && b.all_publishers) OR (a.all_publishers IS NULL AND b.all_publisherse IS NULL))
+    AND a.manga = FALSE AND b.manga = FALSE
+    AND ((a.titlepart = b.titlepart) OR (a.titlepart IS NULL AND b.titlepart IS NULL))
+    AND ((a.titlepartname = b.titlepartname) OR (a.titlepartname IS NULL AND b.titlepartname IS NULL))
+    AND (
+          (a.record > b.record AND dedupe_setting('dedupe_type') = 'inclusive')
+            OR
+          (a.staged = FALSE AND b.staged = TRUE AND EXISTS (SELECT 1 FROM dedupe_features WHERE name = 'dedupe_type' AND value IN ('subset','migration')))
+        )
+    AND dedupe_setting('merge tacs isbn and pubdate') = 'TRUE'
+;
+
 -- stock manga match set based on publisher list 
 INSERT INTO pairs (merge_set,records,match_set)
 SELECT
