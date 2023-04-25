14d90db42b515080eeb44daef0834c85b372a2ec
Rogan Hamby <rogan.hamby@equinoxinitiative.org>
2023-04-25 11:15:47 -0400

    adding oclc match set

--- sql/dedupe/custom/07_pairs_print.sql
+++ sql/dedupe/custom/07_pairs_print.sql
@@ -32,6 +32,20 @@ WHERE
     AND ( titlepartname OR titlepartname IS NULL )
     AND ( dedupe_setting('page variation') IS NULL OR pages ) -- pages will be null if no setting 
     AND ( dedupe_setting('centimeter variation') IS NULL or centimeters )
+
+UPDATE pairs SET merge_set = ARRAY_APPEND(merge_set,'tacs oclc')
+WHERE
+    search_format_str
+    AND author
+    AND can_have_copies
+    AND NOT avdisc_flag
+    AND oclc_values
+    AND NOT manga
+    AND ( titlepart OR titlepart IS NULL )
+    AND ( titlepartname OR titlepartname IS NULL )
+    AND ( dedupe_setting('page variation') IS NULL OR pages ) -- pages will be null if 
+no setting 
+    AND ( dedupe_setting('centimeter variation') IS NULL or centimeters )
 ;
 
 -- stock manga match set based on publisher list 
