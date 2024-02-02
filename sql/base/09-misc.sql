3793ca3df9d07c5b8eeb8b6c1a8a7914bac1010a
Rogan Hamby <rogan.hamby@equinoxinitiative.org>
2024-02-02 07:53:17 -0500

    New wrapper function to discourage use of bare TRUNCATE commands
    Add a migration_tools.safer_truncate() function that checks
    whether the table to truncate is in a child/parent inheritance
    relationship, is in a standard Evergreen schema, and whether
    it does not exists at all. If any of those conditions are met,
    do not truncate the table.
    
    Closes #36
    
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

--- sql/base/09-misc.sql
+++ sql/base/09-misc.sql
@@ -89,3 +89,42 @@ BEGIN
     RETURN TRUE;
 END
 $function$;
+
+
+DROP FUNCTION IF EXISTS migration_tools.safer_truncate(TEXT,TEXT);
+CREATE OR REPLACE FUNCTION migration_tools.safer_truncate(target_schema TEXT,target_table TEXT) RETURNS TEXT
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+    valid_truncate BOOLEAN DEFAULT FALSE;
+    is_child_or_parent BOOLEAN DEFAULT FALSE;
+    truncate_statement TEXT;
+BEGIN
+    -- reject if not valid
+    EXECUTE 'SELECT EXISTS(SELECT 1 FROM information_schema.tables WHERE table_schema = $1 AND table_name = $2)' 
+        INTO valid_truncate USING target_schema, target_table;
+    -- RAISE EXCEPTION 'value is %', valid_truncate;
+    IF NOT valid_truncate THEN RETURN 'invalid schema or table'; END IF;
+   
+    -- reject if part of an evergreen schema 
+    IF LOWER(target_schema) IN ('acq','action','action_trigger','actor','asset','auditor',
+        'authority','biblio','booking','config','container','evergreen','extend_reporter',
+        'metabib','money','oai','offline','permission','query','rating','reporter','search',
+        'serial','staging','stats','unapi','url_verify','vandelay') 
+    THEN RETURN 'you may not truncate a table in a production schema';
+    END IF;
+
+    -- reject if a child or parent as that gets tricky
+    -- and in migrations I only truncate stand alone tables  
+    SELECT 1 
+    FROM pg_catalog.pg_inherits 
+    WHERE inhparent::regclass::text = CONCAT_WS('.',target_schema,target_table)
+        OR inhrelid::regclass::text = CONCAT_WS('.',target_schema,target_table)
+    INTO is_child_or_parent;
+    IF is_child_or_parent THEN RETURN 'do not truncate a parent or child table'; END IF;
+
+    truncate_statement := 'TRUNCATE TABLE ' || target_schema || '.' || target_table;
+    EXECUTE truncate_statement;
+    RETURN 'success';
+END
+$function$;
