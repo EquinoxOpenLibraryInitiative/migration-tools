ec86bb5bcaf36ecc17f834b076c2099eb7f6d005
Rogan Hamby <rogan.hamby@equinoxinitiative.org>
2025-08-26 22:37:41 -0400

    make marking a bib's x_merget_to target also set x_migrat to false and an x_migrate_reason to make it more robust

--- sql/dedupe/custom/13_dedupe_migration.sql
+++ sql/dedupe/custom/13_dedupe_migration.sql
@@ -10,8 +10,9 @@ ALTER TABLE m_biblio_record_entry_legacy ADD COLUMN x_merge_to BIGINT;
 if this is being done for staged records there is no actual merging to do, just new targets for the bibs 
 */
 
-    
 WITH tmp AS (SELECT lead_record, UNNEST(records) AS record FROM groups)
-UPDATE m_biblio_record_entry_legacy a SET x_merge_to = t.lead_record
+UPDATE m_biblio_record_entry_legacy a
+SET x_merge_to = t.lead_record,
+    x_migrate = FALSE,
+    x_migrate_reason = ARRAY_APPEND(x_migrate_reason,'deduplicated record')
 FROM tmp t WHERE t.record = a.id;
-
